581d313ef75e0ae8b8dbc73c044c93cf0df8da62
Emmanuel-Develops <itakpeemma@gmail.com>
2020-10-01 14:55:41 +0100

    added live links and tweaked gsap animation

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ const nav = document.querySelector(".nav-element");
 const headerBg = document.querySelector(".header-bg");
 const profileTransition = document.querySelector(".profile-left");
 const profileTransitionInfo = document.querySelector(".profile-info");
+const tl = gsap.timeline({ defaults: { ease: "powe1.out" } });
 
 navToggle.addEventListener("click", (event) => {
   console.log(event);
@@ -10,11 +11,24 @@ navToggle.addEventListener("click", (event) => {
   headerBg.classList.toggle("header-bg--visible");
 });
 
-window.onload = (event) => {
-  console.log(event);
-  profileTransition.classList.toggle("profile-left--start");
-  profileTransitionInfo.classList.toggle("profile-info--start");
-};
+// Move profile picture section in view from the left
+tl.fromTo(".profile-left", { x: "-100%" }, { x: "0%", duration: 1.6 });
+// Move profile info section in view from the right, at the same time as profile image
+tl.fromTo(".profile-info", { x: "100%" }, { x: "0%", duration: 1.6 }, "-=1.6");
+// Animate the header text
+tl.fromTo(".profile-info-header", { fontSize: "2rem", y: "200%" }, { fontSize: "1.3rem", y: "0%", duration: 1 });
+// Animate hr opacity
+tl.fromTo(".profile-info hr", { opacity: "0" }, { opacity: "1", duration: 0.5 });
+// Animate profile info text to come up into view
+tl.fromTo(".tl-text", { y: "100%", opacity: "0" }, { y: "0%", opacity: "1", duration: 1 });
+// Animate buttons opacity
+tl.fromTo(".btn", { opacity: "0" }, { opacity: "1", duration: 1 });
+
+// window.onload = (event) => {
+//   console.log(event);
+// profileTransition.classList.toggle("profile-left--start");
+//   profileTransitionInfo.classList.toggle("profile-info--start");
+// };
 
 // profileTransition.classList.toggle("profile-left--start");
 // profileTransitionInfo.classList.toggle("profile-info--start");
